8d939cd2883b792909ef93e4f868a4c4e7b0dd86
Lucas <lucaslincoln0706@gmail.com>
2022-07-07 03:09:17 -0400

    drop page structure

--- src/components/Uploader.tsx
+++ src/components/Uploader.tsx
@@ -2,9 +2,12 @@ import React from "react";
 import "react-dropzone-uploader/dist/styles.css";
 import Dropzone from "react-dropzone-uploader";
 import { useState } from "react";
+import './style.css';
 
 const Uploader = () => {
   const [file, setFile] = useState();
+  const [title, setTitle] = useState();
+  const [description, setDescription] = useState();
   const axios = require("axios").default;
   const endpoint:string = "https://pa3qluw7el.execute-api.us-east-1.amazonaws.com/default/uploadFile";
 
@@ -14,6 +17,14 @@ const Uploader = () => {
     console.log(meta.name);
     setFile(meta.name);
   };
+
+  const handleChangeTitle = (event:any) => {
+    setTitle(event.value);
+  }
+
+  const handleChangeDescription = (event:any) => {
+    setDescription(event.value);
+  }
 
   const handleSubmit = async (files:any) => {
     const f = files[0];
@@ -32,14 +43,14 @@ const Uploader = () => {
   };
 
   return (
-    <form onSubmit={handleSubmit}>
-      <label>
+    <form onSubmit={handleSubmit} className="form">
+      <label className="flex justify-between flex-col">
         title:
-        <input type="text"/>
+        <input type="text" onChange={handleChangeTitle}/>
       </label>
-      <label>
+      <label className="flex justify-between flex-col">
         description:
-        <input type="text"/>
+        <textarea name="description" onChange={handleChangeDescription}/>
       </label>
       <Dropzone
         onChangeStatus={handleChangeStatus}
@@ -49,6 +60,7 @@ const Uploader = () => {
         canCancel={false}
         inputContent="Drop A File"
       />
+      <input type="submit" value="Submit" />
     </form>
   );
 };
